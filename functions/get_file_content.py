761256be2d0e9c6a95d6137bb0a1b5ca7d10a662
wbhemingway <william.b.hemingway@gmail.com>
2025-10-02 20:37:16 -0400

    Changed api key, refactored slightly, added aiagent schema functions

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,6 +1,6 @@
 import os
-
-from .config import MAX_CHARACTERS
+from google.genai import types
+from config import MAX_CHARACTERS
 
 
 def get_file_content(working_directory, file_path):
@@ -18,3 +18,18 @@ def get_file_content(working_directory, file_path):
         return file_content
     except Exception as e:
         return f"Error: {e}"
+    
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARACTERS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
--- main.py
+++ main.py
@@ -1,31 +1,61 @@
-import os
 import sys
-
-from dotenv import load_dotenv
+import os
 from google import genai
 from google.genai import types
+from dotenv import load_dotenv
 
-if len(sys.argv) < 2:
-    sys.exit(1)
-user_prompt = sys.argv[1]
+from prompts import system_prompt
+from call_function import available_functions
 
-load_dotenv()
 
-messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
-)
-print(response.text)
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
-if "--verbose" in sys.argv[2:]:
-    meta_data = response.usage_metadata
-    prompt_tokens = meta_data.prompt_token_count
-    response_tokens = meta_data.candidates_token_count
-    print(f"User prompt: {user_prompt}")
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
+        sys.exit(1)
 
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
+
+if __name__ == "__main__":
+    main()
